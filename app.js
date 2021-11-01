9ba72f6513df320010a31591d58bad9f3ba70996
Magda <meg.nowak27@gmail.com>
2021-11-01 17:51:25 +0100

    add events to icons

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const optionsBtn = document.querySelector(".stopwatch__icon-options");
 const optionsPopup = document.querySelector(".options");
+const openInstructionBtn = document.querySelector(".stopwatch__icon-instruction");
+const instructionPopup = document.querySelector(".instruction");
+const closeInstructionBtn = document.querySelector(".instruction__btn");
 
 const openOptionsPopup = () => {
   optionsPopup.classList.toggle("active");
 };
+const openInstructionPopup = () => {
+  instructionPopup.classList.add("active");
+};
+const closeInstructionPopup = () => {
+  instructionPopup.classList.remove("active");
+};
 
 optionsBtn.addEventListener("click", openOptionsPopup);
+openInstructionBtn.addEventListener("click", openInstructionPopup);
+closeInstructionBtn.addEventListener("click", closeInstructionPopup);
